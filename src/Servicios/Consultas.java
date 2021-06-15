2e4b740cfbd7d07b9c563d5cbe08a9b5865999a8
WikyPassu <alan_passucci@hotmail.com>
2021-06-15 20:29:39 -0300

    2 y 3a

--- src/Servicios/Consultas.java
+++ src/Servicios/Consultas.java
@@ -11,21 +11,20 @@ import Utilidades.UBean;
 public class Consultas {
 	
 	public static String guardar(Object o) {
-		String query = "insert into ".concat(o.getClass().getAnnotation(Tabla.class).nombre()).concat(" (");
 		ArrayList<Field> fields = UBean.obtenerAtributos(o);
+		String query = "insert into ".concat(o.getClass().getAnnotation(Tabla.class).nombre()).concat(" (");
+		String nombreCampos = "";
+		String contenidoCampos = "";
 		
 		for(Field f: fields) {
 			if(f.getAnnotation(Id.class) == null) {
-				query = query.concat(f.getAnnotation(Columna.class).nombre()).concat(", ");
+				nombreCampos = nombreCampos.concat(f.getAnnotation(Columna.class).nombre()).concat(", ");
+				contenidoCampos = contenidoCampos.concat(UBean.ejecutarGet(o, f.getName()).toString()).concat(", ");
 			}
 		}
-		query = query.substring(0, query.length() - 2).concat(") values(");
-		for(Field f: fields) {
-			if(f.getAnnotation(Id.class) == null) {
-				query = query.concat(UBean.ejecutarGet(o, f.getName()).toString()).concat(", ");	
-			}
-		}
-		query = query.substring(0, query.length() - 2).concat(")");
+		nombreCampos = nombreCampos.substring(0, nombreCampos.length() - 2).concat(") values (");
+		contenidoCampos = contenidoCampos.substring(0, contenidoCampos.length() - 2).concat(")");
+		query = query.concat(nombreCampos).concat(contenidoCampos);
 		
 		return query;
 	}
