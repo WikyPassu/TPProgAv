8e12d52e335d39590e4b6a4db09feeafe02df292
WikyPassu <alan_passucci@hotmail.com>
2021-06-16 05:18:30 -0300

    End 1,2,3,4,7

--- src/Program/Program.java
+++ src/Program/Program.java
@@ -16,19 +16,21 @@ public class Program {
 		
 		//Crear objeto de tipo Persona
 		Persona p = new Persona();
-		/*
+		
 		//Test 1a obtenerAtributos
 		ArrayList<Field> fields = UBean.obtenerAtributos(p);
-		
+		System.out.println("Test 1a obtenerAtributos");
 		for(Field f: fields) {
 			System.out.println(f.getName());
 		}
 		
 		//Test 1b ejecutarSet
+		System.out.println("Test 1b ejecutarSet");
 		UBean.ejecutarSet(p, "nombre", "Carlos");
 		UBean.ejecutarSet(p, "apellido", "Gomez");
 		
 		//Test 1c ejecutarGet
+		System.out.println("Test 1c ejecutarGet");
 		System.out.println(UBean.ejecutarGet(p, "nombre"));
 		System.out.println(UBean.ejecutarGet(p, "apellido"));
 		
@@ -36,23 +38,30 @@ public class Program {
 		UConexion cn = UConexion.generarObjeto();
 		
 		//Test 3a guardar
-		System.out.println(Consultas.guardar(p));
+		System.out.println("Test 3a guardar");
+		Consultas.guardar(p);
 		
 		//Test 3b modificar
-		UBean.ejecutarSet(p, "id", 2);
+		System.out.println("Test 3b modificar");
+		UBean.ejecutarSet(p, "id", 3);
 		UBean.ejecutarSet(p, "nombre", "Maria");
 		UBean.ejecutarSet(p, "apellido", "Lopez");
-		System.out.println(Consultas.modificar(p));
-		*/
+		Consultas.modificar(p);
+		
 		//Test 3c eliminar
-		UBean.ejecutarSet(p, "id", 2);
-		System.out.println(Consultas.eliminar(p));
+		System.out.println("Test 3c eliminar");
+		UBean.ejecutarSet(p, "id", 1);
+		Consultas.eliminar(p);
+		
+		//Test 3d obtenerPorId
+		System.out.println("Test 3d obtenerPorId");
+		System.out.println((Persona)Consultas.obtenerPorId(Persona.class, 3));
 		
 		//Test 7 obtenerTodos
+		System.out.println("Test 7 obtenerTodos");
 		ArrayList<Persona> personas = (ArrayList<Persona>) Consultas.obtenerTodos(Persona.class);
 		for(Persona per: personas) {
 			System.out.println(per);
 		}
-		
 	}
 }
--- src/Servicios/Consultas.java
+++ src/Servicios/Consultas.java
@@ -16,12 +16,11 @@ import Utilidades.UConexion;
 
 public class Consultas {
 	
-	public static String guardar(Object o) {
+	public static void guardar(Object o) {
 		ArrayList<Field> fields = UBean.obtenerAtributos(o);
 		String query = "insert into ".concat(o.getClass().getAnnotation(Tabla.class).nombre()).concat(" (");
 		String nombreCampos = "";
 		String contenidoCampos = "";
-		
 		for(Field f: fields) {
 			if(f.getAnnotation(Id.class) == null) {
 				nombreCampos = nombreCampos.concat(f.getAnnotation(Columna.class).nombre()).concat(", ");
@@ -36,7 +35,7 @@ public class Consultas {
 		nombreCampos = nombreCampos.substring(0, nombreCampos.length() - 2).concat(") values (");
 		contenidoCampos = contenidoCampos.substring(0, contenidoCampos.length() - 2).concat(")");
 		query = query.concat(nombreCampos).concat(contenidoCampos);
-		
+		System.out.println(query);
 		try {
 			UConexion.abrirConexion();
 			PreparedStatement pst = UConexion.c.prepareStatement(query);
@@ -45,17 +44,14 @@ public class Consultas {
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-		return query;
 	}
 	
-	public static String modificar(Object o) {
+	public static void modificar(Object o) {
 		ArrayList<Field> fields = UBean.obtenerAtributos(o);
 		String query = "update ".concat(o.getClass().getAnnotation(Tabla.class).nombre()).concat(" set ");
 		String camposValor = "";
 		String nombreCampoId = "";
 		String contenidoCampoId = "";
-		
 		for(Field f: fields) {
 			if(f.getAnnotation(Id.class) == null) {
 				if(f.getType() == String.class) {
@@ -72,7 +68,7 @@ public class Consultas {
 		}
 		camposValor = camposValor.substring(0, camposValor.length() - 2);
 		query = query.concat(camposValor).concat(" where ").concat(nombreCampoId).concat("=").concat(contenidoCampoId);
-		
+		System.out.println(query);
 		try {
 			UConexion.abrirConexion();
 			PreparedStatement pst = UConexion.c.prepareStatement(query);
@@ -81,20 +77,18 @@ public class Consultas {
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-		return query;
 	}
 	
-	public static String eliminar(Object o) {
+	public static void eliminar(Object o) {
 		ArrayList<Field> fields = UBean.obtenerAtributos(o);
 		String query = "delete from ".concat(o.getClass().getAnnotation(Tabla.class).nombre()).concat(" where ");
-		
 		for(Field f: fields) {
 			if(f.getAnnotation(Id.class) != null) {
 				query = query.concat(f.getAnnotation(Columna.class).nombre()).concat("="+UBean.ejecutarGet(o, f.getName()));
+				break;
 			}
 		}
-		
+		System.out.println(query);
 		try {
 			UConexion.abrirConexion();
 			PreparedStatement pst = UConexion.c.prepareStatement(query);
@@ -103,8 +97,40 @@ public class Consultas {
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-		return query;
+	}
+	
+	public static Object obtenerPorId(Class<?> c, Object id) {
+		Object o = null;
+		try {
+			Constructor[] constructores = c.getConstructors();
+			for(Constructor con: constructores) {
+				if(con.getParameterCount() == 0) {
+					o = con.newInstance();
+					break;
+				}
+			}
+			ArrayList<Field> fields = UBean.obtenerAtributos(o);
+			String query = "select * from ".concat(c.getAnnotation(Tabla.class).nombre()).concat(" where ");
+			for(Field f: fields) {
+				if(f.getAnnotation(Id.class) != null) {
+					query = query.concat(f.getAnnotation(Columna.class).nombre()).concat("="+id.toString());
+					break;
+				}
+			}
+			System.out.println(query);
+			UConexion.abrirConexion();
+			PreparedStatement pst = UConexion.c.prepareStatement(query);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				for(Field f: fields) {
+					UBean.ejecutarSet(o, f.getName(), rs.getObject(f.getAnnotation(Columna.class).nombre()));
+				}
+			}
+			UConexion.cerrarConexion();
+		} catch(SQLException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return o;
 	}
 	
 	public static ArrayList<?> obtenerTodos(Class<?> c){
@@ -118,8 +144,10 @@ public class Consultas {
 					break;
 				}
 			}
+			String query = "select * from "+c.getAnnotation(Tabla.class).nombre();
+			System.out.println(query);
 			UConexion.abrirConexion();
-			PreparedStatement pst = UConexion.c.prepareStatement("select * from "+c.getAnnotation(Tabla.class).nombre());
+			PreparedStatement pst = UConexion.c.prepareStatement(query);
 			ResultSet rs = pst.executeQuery();
 			ArrayList<Field> fields = UBean.obtenerAtributos(o);
 			while(rs.next()) {
